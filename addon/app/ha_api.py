4f8faedf4451d5fdcdf851d32ced34e0dcf6b353
Chris <chrisgozd92@gmail.com>
2026-08-24 13:47:50 -0500

    log

--- addon/app/ha_api.py
+++ addon/app/ha_api.py
@@ -109,6 +109,7 @@ async def get_device_board() -> list[dict]:
     devices = _entries(raw_devices, "device", "id", "device_id")
     
     _LOGGER.info(devices)
+    _LOGGER.info(raw_entries)
 
     areas = _entries(raw_areas, "area", "area_id", "id")
     floors = _entries(raw_floors, "floor", "floor_id", "id")
